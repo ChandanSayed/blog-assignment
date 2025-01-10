46a76e9106f5ca0e89e58a6fda90181fa024fa05
Chandan Sayed <hangzhou635@gmail.com>
2025-01-10 15:56:43 +0600

    update

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,4 @@
-const env = process.env.NODE_ENV || 'development'
 
 export const config = {
-  baseUrl: env === 'production' 
-    ? process.env.NEXT_PUBLIC_BASE_URL 
-    : 'http://localhost:3000',
+  baseUrl: 'http://localhost:3000',
 } 
